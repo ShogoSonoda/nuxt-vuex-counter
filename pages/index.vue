1546d68c1d953a2db7fcf3c61183684c1ad36022
ShogoSonoda <sonoponsyougo@gmail.com>
2021-06-29 22:35:41 +0900

    カウンターコンポーネントの利用

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <Tutorial/>
+  <section class="container">
+    <div>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+      <Counter></Counter>
+      <hr>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {}
+import Counter from '~/compornents/Counter.vue'
+
+export default {
+  components: {
+    Counter
+  }
+}
 </script>
